857dbd65a15902b6bd4fc7e83313f625cfd1ba57
Luca <93129094+LucaRomio@users.noreply.github.com>
2022-05-22 03:52:13 +0200

    Add files via upload

---
+++ e-shop-manager/src/test/java/it/unipd/mtss/AppTest.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// [Gabriel] [Rovesti] [2009088]
+// [Luca] [Romio] [2014028]
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void testAppTest(){
+        Assertions.assertTrue(true);
+    }
+}
