162969e53552bdf3cb30c30370a31ea4be1b6483
Luca <93129094+LucaRomio@users.noreply.github.com>
2022-05-22 03:55:07 +0200

    Delete src directory

--- src/main/java/it/unipd/mtss/Bill.java
+++
@@ -1,9 +0,0 @@
-package it.unipd.mtss;
-
-import java.util.List;
-
-import it.unipd.mtss.exception.BillException;
-
-public interface Bill {
-	public double getOrderPrice(List<EItem> itemsOrdered, User user) throws BillException;
-}
